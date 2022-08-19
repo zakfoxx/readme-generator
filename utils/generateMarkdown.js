d76860aae63fa239cd3abb4bdefa88d621cb40e3
Zechariah Foxx <zak.foxx@gmail.com>
2022-08-19 16:03:07 -0400

    work markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,6 +13,31 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+## Description
+${data.description}
+
+## Table of Contents (Optional)
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${data.license}
+
+## Badges
+${data.badges}
+## How to Contribute
+
+## Tests
 
 `;
 }
